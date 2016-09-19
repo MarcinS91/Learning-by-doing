965e24a9774cab277ac6d624be728bf187c3a591
MarcinS91 <szewc.marcin91@gmail.com>
2016-09-19 18:56:44 +0200

    commit

--- Learning by doing/src/FileLoader/Loader.java
+++ Learning by doing/src/FileLoader/Loader.java
@@ -1,9 +1,25 @@
 package FileLoader;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Loader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		File file = new File("");
+
+		try {
+			Scanner sc = new Scanner(file).useDelimiter(":");
+
+			while (sc.hasNextLine()) {
+				String line = sc.nextLine();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 	}
 
