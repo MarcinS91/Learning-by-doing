9daf6dbad2b725abe0b127692dd5ed249ab38b75
MarcinS91 <szewc.marcin91@gmail.com>
2016-09-20 23:15:13 +0200

    commit

---
+++ Learning by doing/src/commonDivisor/CommonDivisor.java
@@ -0,0 +1,30 @@
+package commonDivisor;
+
+import java.util.Scanner;
+
+public class CommonDivisor {
+
+	public static int div(int a, int b) {
+		int r = a % b;
+		while (r != 0) {
+			a = b;
+			b = r;
+			r = a % b;
+		}
+		return b;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Podaj wartosc liczby pierwszej:");
+		int a = sc.nextInt();
+
+		System.out.println("Podaj wartosc drugiek liczby:");
+		int b = sc.nextInt();
+
+		System.out.println(" Najwiekszy wspolny dziennik licz wynosi: " + div(a, b));
+
+	}
+
+}
